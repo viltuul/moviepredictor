bbe00a5c23f8c7b1b66a8b06448245e3f31bc75e
Psion <andreas.andra@helsinki.fi>
2017-10-24 03:55:50 +0300

    random forest started

---
+++ randomforest.py
@@ -0,0 +1,30 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn import preprocessing
+
+df = pd.read_csv('parsedRoger Corman_main_business_vote details_keywords_taglines_trivia_release dates.csv', index_col = False) 
+
+#fills missing values with means
+for val in ["rating", "votes", "year", "runtimes", "budget"]:
+    df[val].fillna(df[val].mean(), inplace=True)
+
+clf = RandomForestClassifier(n_estimators = 100, max_depth = 4)
+
+#floats can't be used as target vector with random forest
+lab_enc = preprocessing.LabelEncoder()
+df["rating"] = lab_enc.fit_transform(df["rating"])
+
+#sets target and data as numpy array
+target = df["rating"].values
+data = df[["votes", "year", "runtimes", "budget"]].values
+
+# Split the data and target into training/testing sets
+X_train, X_test, y_train, y_test = train_test_split(data, target, train_size = 0.8, test_size = 0.2)
+clf.fit(X_train, y_train)
+
+
+print clf.feature_importances_
+y_pred = clf.predict(X_test)
+print y_pred[0:5]/10.0
--- ratings_predictor.py
+++ ratings_predictor.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-
-import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-
+from sklearn import preprocessing
 
 df = pd.read_csv('parsedRoger Corman_main_business_vote details_keywords_taglines_trivia_release dates.csv', index_col = False) 
 
 #fills missing values with means
-for val in ["rating", "votes", "year"]:
+for val in ["rating", "votes", "year", "runtimes", "budget"]:
     df[val].fillna(df[val].mean(), inplace=True)
-    
-
-categorical_values = ["role", "kind", "genres"]
-
-# Turn categorical values into numbers instead of strings
-for val in categorical_values:
-    df[val] = df[val].astype('category')
-
-cat_columns = df.select_dtypes(['category']).columns
-df[cat_columns] = df[cat_columns].apply(lambda x: x.cat.codes)
 
 #sets target and data as numpy array
 target = df['rating'].values
@@ -31,29 +20,24 @@ data = df[["year"]].values   #for multiple regression  data = df[["role", "kind"
 # Split the data and target into training/testing sets
 X_train, X_test, y_train, y_test = train_test_split(data, target, train_size = 0.8, test_size = 0.2)
 
-# Create linear regression object
 regr = linear_model.LinearRegression()
+clf = RandomForestClassifier(n_estimators = 100, max_depth = 4)
 
 # Train the model using the training sets
 regr.fit(X_train, y_train)
 
+#floats can't be used as target vector with random forest
+lab_enc = preprocessing.LabelEncoder()
+y_train = lab_enc.fit_transform(y_train)
+y_test = lab_enc.fit_transform(y_test)
+
+# Train the model using the training sets
+clf.fit(X_train, y_train)
+
 # Make predictions using the testing set
 y_pred = regr.predict(X_test)
-print y_pred[0:5]
+print "Linear regression predictions"
+print regr.predict(X_test)[0:5]
 
-# The coefficients
-print('Coefficients: \n', regr.coef_)
-# The mean squared error
-print("Mean squared error: %.2f"
-      % mean_squared_error(y_test, y_pred))
-# Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % r2_score(y_test, y_pred))
-
-# Plot outputs
-plt.scatter(X_test, y_test,  color='black')
-plt.plot(X_test, y_pred, color='blue', linewidth=3)
-
-plt.xticks(())
-plt.yticks(())
-
-plt.show()
+print "Random forest predictions"
+print clf.predict(X_test)[0:5]/10.0
